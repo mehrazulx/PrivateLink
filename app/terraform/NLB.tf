6d05b1271debfa34fead4e3af02e3cb7c06e20ba
mehrazulx <mhoque@register-iri.com>
2026-03-25 14:44:47 -0400

    nlb

--- app/terraform/NLB.tf
+++ app/terraform/NLB.tf
@@ -2,7 +2,7 @@
 
 # Load Balancer________________________________________________________________________________
 resource "aws_lb" "ECS_NLB" {
-name               = "ECS-NLB"
+name               = "ECS-NLB-terraform"
 internal           = true
 load_balancer_type = "network"
 subnets            = [aws_subnet.existing_private.id]
