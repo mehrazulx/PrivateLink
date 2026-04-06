cab2f904625b08b503873990f08d444c4ab8cc32
mehrazulx <mhoque@register-iri.com>
2026-04-06 12:29:59 -0400

    removed unnecessary Ports and harden security

--- app/terraform/SecurityGroup.tf
+++ app/terraform/SecurityGroup.tf
@@ -5,63 +5,18 @@ resource "aws_security_group" "public_sg" {
   vpc_id      = aws_vpc.Public.id
 
   # Inbound Rules
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    self            = true
-  }
 
-  # Self-referencing port 3000
-  ingress {
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    cidr_blocks = ["10.1.0.0/16"]
-  }
-ingress {
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
 
-  # Port 8000 rules
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["10.1.0.0/16"]
-  }
-
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
 
   # Port 80 rules
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.1.0/24"]
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.1.0.0/16"]
-  }
+ 
 
   # Outbound rule
   egress {
@@ -84,73 +39,16 @@ resource "aws_security_group" "private_sg" {
 
   # Inbound Rules
   ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    self            = true
-  }
-
-  # Self-referencing port 3000
-  ingress {
     from_port       = 3000
     to_port         = 3000
     protocol        = "tcp"
-    cidr_blocks = ["10.1.0.0/16"]
-  }
-ingress {
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.1.1.0/24"]
   }
 
-  # Port 8000 rules
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["10.1.0.0/16"]
-  }
 
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  # Port 80 rules
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.1.0.0/16"]
-  }
-
-  # Outbound rule
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  
 
   tags = {
     Name = "SF2"
   }
 }
-
